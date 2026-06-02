84b4160b36f395ed554da09cbafe54fab60ae8d2
Th0Kh4nh <khanh14042004@gmail.com>
2026-06-02 10:55:20 +0700

    sua setting core backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-o!=c0x=@tjk_3xex*_+fuza&v5t01yks^gjl&ie0ua(1wwothe'
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='unsafe-dev-secret')
 DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -110,15 +110,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='football_booking_db'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+           'NAME': config('DB_NAME', default='4man_db'),
+           'USER': config('DB_USER', default='root'),
+           'PASSWORD': config('DB_PASSWORD', default=''),
+           'HOST': config('DB_HOST', default='localhost'),
+           'PORT': config('DB_PORT', default='3306'),
     }
 }
 
